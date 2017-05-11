8b65fc7a2b8aa1abcbe9f85c322bc8595ca8380a
Yauhen <stan01@tut.by>
2017-05-11 10:11:34 +0300

    chapter 04 abstract factory

---
+++ com/ch04Factory/ingridients/impl/ny/NYPizzaIngredientFactory.java
@@ -0,0 +1,38 @@
+package com.ch04Factory.ingridients.impl.ny;
+
+import com.ch04Factory.ingridients.*;
+
+/**
+ * Created by Aspire on 02.05.2017.
+ */
+public class NYPizzaIngredientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new ThinCrustDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new MarinaraSauce();
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return new ReggianoCheese();
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        return new Veggies[]{new Garlic(), new Onion(), new Mushroom(), new RedPepper()};
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    @Override
+    public Clams createClams() {
+        return new FreshClams();
+    }
+}
