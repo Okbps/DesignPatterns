058eea4af07911961cda56d5fd2c9d73be51cdaf
Yauhen <stan01@tut.by>
2017-05-31 22:11:41 +0300

    chapter 12 dj model + view

---
+++ com/ch12Compound/djview/BPMObserver.java
@@ -0,0 +1,5 @@
+package com.ch12Compound.djview;
+  
+public interface BPMObserver {
+	void updateBPM();
+}
---
+++ com/ch12Compound/djview/BeatModelInterface.java
@@ -0,0 +1,24 @@
+package com.ch12Compound.djview;
+
+/**
+ * Created by Aspire on 31.05.2017.
+ */
+public interface BeatModelInterface {
+    void initialize();
+
+    void on();
+
+    void off();
+
+    void setBPM(int bpm);
+
+    int getBPM();
+
+    void registerObserver(BeatObserver o);
+
+    void removeObserver(BeatObserver o);
+
+    void registerObserver(BPMObserver o);
+
+    void removeObserver(BPMObserver o);
+}
